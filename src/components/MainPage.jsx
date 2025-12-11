2460b478a42f49bcb6a5c7f931aa00741ea30f43
shashank saini <meanzzz38@gmail.com>
2025-12-11 23:27:15 +0530

    Add WhyChoose compone

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,11 +6,10 @@ import HeroSection from "./HeroSection";
 
 import OurFeatures from "./OurFeatures";
 import GetInTouch from "./GetinTouch";
-import ChatBot from "./Chatbot";
+// import ChatBot from "./ChatBot";
+import ChatWidget from "./Chatbot";
+// import WhyChoose from "./whyChoose";
 import WhyChoose from "./whyChoose";
-
-// import whyChoose from "./whyChoose";
-// import WhyChoose from "./WhyChoose";
 
 function MainPage() {
   return (
@@ -26,6 +25,7 @@ function MainPage() {
       <WhyChoose/>
         <OurFeatures />
         <ChatBot/>
+<ChatWidget/>
         <GetInTouch/>
      
    
